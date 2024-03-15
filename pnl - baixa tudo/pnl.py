7c8411dd9cef05657bff67cff49f69e203c88a88
luc <160556539+lucgabriel22@users.noreply.github.com>
2024-03-15 20:11:28 -0300

    ✨feat: Adicionado novas funcionalidades
    Adicionado baixar música/vídeo

--- pnl - baixa tudo/pnl.py
+++ pnl - baixa tudo/pnl.py
@@ -1,21 +1,33 @@
 from pytube import YouTube
 import tkinter
 import customtkinter
+import os
 
 
-
-
-def IniciarDownload():
+def IniciarDownloadMusica():
     try:
         ytLink = link.get()
         ytObject = YouTube(ytLink, on_progress_callback=on_progress)
-        musica = ytObject.streams.get_audio_only()
+        musica = ytObject.streams.filter(only_audio=True).first()
+        file_path = f"{ytObject.title}.mp3"
+        musica.download(output_path='.', filename=file_path)
         titulo.configure(text=ytObject.title, text_color='white')
-        titulo.configure(text='')
-        musica.download()
         ultimalabel.configure(text='Download Concluído com sucesso!', text_color='green')
-    except:
-        ultimalabel.configure(text='Url não reconhecida, Download não concluído. Tente novamente.', text_color='red')
+    except Exception as e:
+        ultimalabel.configure(text=f'Erro: {str(e)}', text_color='red')
+
+
+def IniciarDownloadVideo():
+    try:
+        ytLink = link.get()
+        ytObject = YouTube(ytLink, on_progress_callback=on_progress)
+        video = ytObject.streams.get_highest_resolution()
+        file_path = f"{ytObject.title}.mp4"
+        video.download(output_path='.', filename=file_path)
+        titulo.configure(text=ytObject.title, text_color='white')
+        ultimalabel.configure(text='Download Concluído com sucesso!', text_color='green')
+    except Exception as e:
+        ultimalabel.configure(text=f'Erro: {str(e)}', text_color='red')
 
 def on_progress(stream, chunk, bytes_remaining):
     tamanho_total = stream.filesize
@@ -23,22 +35,20 @@ def on_progress(stream, chunk, bytes_remaining):
     porcentagem = bytes / tamanho_total * 100
     por = str(int(porcentagem))
     barra_progresso2.configure(text=por + '%')
-    barra_progresso2.update()
-
+    barra_progresso2.update() 
     barra_progresso.set(float(porcentagem) / 100)
-    
 
-# Configurações Iniciais TKINTER
+# < ==========Configs Initials ========== >
 customtkinter.set_appearance_mode('System')
 customtkinter.set_default_color_theme('blue')
-
 
 app = customtkinter.CTk()
 app.geometry('720x480')
 app.title('Paneleiros Downloader')
 
 
-titulo = customtkinter.CTkLabel(app, text='Coloque o link da sua música que deseja baixar')
+# < ========== TITULO DO APP ========== >
+titulo = customtkinter.CTkLabel(app, text='Coloque o link da sua música/vídeo que deseja baixar')
 titulo.pack(padx=10, pady=10)
 
 var = tkinter.StringVar()
@@ -48,6 +58,8 @@ link.pack()
 ultimalabel = customtkinter.CTkLabel(app, text='')
 ultimalabel.pack()
 
+
+# < ========== BARRA DE PROGRESSO ========== >
 barra_progresso = customtkinter.CTkLabel(app, text='0%')
 barra_progresso.pack()
 
@@ -55,8 +67,14 @@ barra_progresso2 = customtkinter.CTkProgressBar(app, width=400)
 barra_progresso2.set(0)
 barra_progresso2.pack(padx=10, pady=10)
 
-download = customtkinter.CTkButton(app, text='Iniciar Download', command=IniciarDownload)
-download.pack(padx=10, pady=10)
 
-                                                                                    
+
+# < ==========Botão de Download em Formato MP3 ========== >
+downloadmusica = customtkinter.CTkButton(app, text='Iniciar Download em MP3', command=IniciarDownloadMusica)
+downloadmusica.pack(padx=10, pady=10)
+
+# < ==========Botão de Download em Formato MP4 ========== >
+downloadvideo = customtkinter.CTkButton(app, text='Iniciar Download em MP4', command=IniciarDownloadVideo)
+downloadvideo.pack(padx=10, pady=10)
+
 app.mainloop()
